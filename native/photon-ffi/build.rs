7f1f92ca72c38e417b02ddee2fa8b99037f0411b
Allen Wyma <allen.wyma@gmail.com>
2021-12-13 15:29:51 +0800

    added photon-ffi lib

---
+++ native/photon-ffi/build.rs
@@ -0,0 +1,16 @@
+use std::env;
+
+fn main() {
+    let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let mut config = cbindgen::Config::default();
+    config.language = cbindgen::Language::C;
+    config.braces = cbindgen::Braces::SameLine;
+    config.cpp_compat = true;
+    config.style = cbindgen::Style::Both;
+    cbindgen::Builder::new()
+        .with_crate(crate_dir)
+        .with_config(config)
+        .generate()
+        .expect("Unable to generate bindings")
+        .write_to_file("libphoton_ffi.h");
+}
